1b3913f7bf3132665a05018f25f685066674eccd
BigBigBigSweetBuns <dadadadoushabao@gmail.com>
2021-06-06 15:41:30 +0800

    转变为面向对象

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,68 +1,69 @@
 var THREE = (window.THREE = require("three"));
 require("three/examples/js/loaders/GLTFLoader");
 
-// 加载模型
-var loader = new THREE.GLTFLoader();
-// loader.load(
-//   "./public/path/DaDouShaBao.glb",
-//   function (gltf) {
-//     scene.add(gltf.scene);
-//   },
-//   undefined,
-//   function (error) {
-//     console.error(error);
-//   }
-// );
-const scene = new THREE.Scene();
-loader.load(
-  "./public/path/just_a_girl/scene.gltf",
-  function (gltf) {
-    gltf.scene.scale.set(0.01, 0.01, 0.01);
-    scene.add(gltf.scene);
-  },
-  // undefined,
-  function (res) {
-    console.log("load", res);
-  },
-  function (error) {
-    console.error("load", error);
+// 初始化画布，相机，模型的对象
+class ThreeTest {
+  x = 0;
+  y = 0;
+  z = 5;
+  rotate = 0; //初始角度
+  rotateAdd = 0.05;// 每次旋转角度
+  constructor() {
+    this.init()
   }
-);
+  init() {
+    this.initScene();
+    this.initRenderer();
+    this.initCamera();
+    this.initModule();
+  }
+  initScene() {
+    this.scene = new THREE.Scene();
+    console.log("scene", this.scene);
+  }
+  initRenderer() {
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setClearColor(0xffff00);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement);
+  }
+  initCamera() {
+    var k = window.innerWidth / window.innerHeight; //窗口宽高比
+    var s = 2;//三维场景显示范围控制系数，系数越大，显示的范围越大
+    this.camera = new THREE.OrthographicCamera(-k * s, k * s, s, -s, 0.1, 1000);
+  }
+  initModule() {
+    let loader = new THREE.GLTFLoader();
+    const that = this;
+    loader.load(
+      "./public/path/just_a_girl/scene.gltf",
+      function (gltf) {
+        gltf.scene.scale.set(0.01, 0.01, 0.01);
+        that.scene.add(gltf.scene);
+      },
+      // undefined,
+      function (res) {
+        console.log("load", res);
+      },
+      function (error) {
+        console.error("load", error);
+      }
+    );
+  }
+}
 
-var renderer = new THREE.WebGLRenderer();
-renderer.setClearColor(0xffff00);
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-var k = window.innerWidth / window.innerHeight; //窗口宽高比
-var s = 2;//三维场景显示范围控制系数，系数越大，显示的范围越大
-var camera = new THREE.OrthographicCamera(-k * s, k * s, s, -s, 0.1, 1000);
-
-// camera.lookAt(new THREE.Vector3(0, 0, 0));
-
-
-// 假设 x，y=1，0
-let x = 0;
-let y = 0;
-let z = 5;
-
-let rotate = 0; //初始角度
-let rotateAdd = .05; //旋转角度
+const threeTest = new ThreeTest();
 
 var animate = function () {
   requestAnimationFrame(animate);
 
-  rotate = rotate < 360 ? rotate + rotateAdd : 0; // 旋转360°
+  threeTest.rotate = threeTest.rotate < 360 ? threeTest.rotate + threeTest.rotateAdd : 0; // 旋转360°
 
-  x = 5 * Math.sin(rotate);
-  z = 5 * Math.cos(rotate);
+  threeTest.camera.position.x = 5 * Math.sin(threeTest.rotate);
+  threeTest.camera.position.z = 5 * Math.cos(threeTest.rotate);
+  threeTest.camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-  camera.position.x = x;
-  camera.position.z = z;
-
-  camera.lookAt(new THREE.Vector3(0, 0, 0));
-
-  renderer.render(scene, camera);
+  threeTest.renderer.render(threeTest.scene, threeTest.camera);
 };
 
 animate();
---
+++ src/pages/index/indexOld.js
@@ -0,0 +1,68 @@
+var THREE = (window.THREE = require("three"));
+require("three/examples/js/loaders/GLTFLoader");
+
+// 加载模型
+var loader = new THREE.GLTFLoader();
+// loader.load(
+//   "./public/path/DaDouShaBao.glb",
+//   function (gltf) {
+//     scene.add(gltf.scene);
+//   },
+//   undefined,
+//   function (error) {
+//     console.error(error);
+//   }
+// );
+const scene = new THREE.Scene();
+loader.load(
+  "./public/path/just_a_girl/scene.gltf",
+  function (gltf) {
+    gltf.scene.scale.set(0.01, 0.01, 0.01);
+    scene.add(gltf.scene);
+  },
+  // undefined,
+  function (res) {
+    console.log("load", res);
+  },
+  function (error) {
+    console.error("load", error);
+  }
+);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setClearColor(0xffff00);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var k = window.innerWidth / window.innerHeight; //窗口宽高比
+var s = 2;//三维场景显示范围控制系数，系数越大，显示的范围越大
+var camera = new THREE.OrthographicCamera(-k * s, k * s, s, -s, 0.1, 1000);
+
+// camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+
+// 假设 x，y=1，0
+let x = 0;
+let y = 0;
+let z = 5;
+
+let rotate = 0; //初始角度
+let rotateAdd = .05; //旋转角度
+
+var animate = function () {
+  requestAnimationFrame(animate);
+
+  rotate = rotate < 360 ? rotate + rotateAdd : 0; // 旋转360°
+
+  x = 5 * Math.sin(rotate);
+  z = 5 * Math.cos(rotate);
+
+  camera.position.x = x;
+  camera.position.z = z;
+
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+  renderer.render(scene, camera);
+};
+
+animate();
