3b986adf4ac3c8b145c3193899c37fd1f6d2c1e3
BigBigBigSweetBuns <dadadadoushabao@gmail.com>
2021-06-06 14:55:23 +0800

    相机旋转模型

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,31 +1,32 @@
 var THREE = (window.THREE = require("three"));
 require("three/examples/js/loaders/GLTFLoader");
 
-var mixers = [];
-var clock = new THREE.Clock();
-(function () {
-  var loader = new THREE.GLTFLoader();
-  loader.load("./public/path/just_a_girl/scene.gltf", function (gltf) {
-    //   loader.load("./public/path/DaDouShaBao.glb", function (gltf) {
-    console.log(gltf);
-    var axesHelper = new THREE.AxesHelper(5);
-    scene.add(axesHelper);
-
-    scene.add(gltf.scene); // 将模型引入three
-
-    // 调用动画
-    var mixer = new THREE.AnimationMixer(gltf.scene.children[2]);
-    await mixer.clipAction(gltf.animations[0]).setDuration(1).play();
-    mixers.push(mixer);
-  });
-})();
-
+// 加载模型
+var loader = new THREE.GLTFLoader();
+// loader.load(
+//   "./public/path/DaDouShaBao.glb",
+//   function (gltf) {
+//     scene.add(gltf.scene);
+//   },
+//   undefined,
+//   function (error) {
+//     console.error(error);
+//   }
+// );
 const scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
+loader.load(
+  "./public/path/just_a_girl/scene.gltf",
+  function (gltf) {
+    gltf.scene.scale.set(0.01, 0.01, 0.01);
+    scene.add(gltf.scene);
+  },
+  // undefined,
+  function (res) {
+    console.log("load", res);
+  },
+  function (error) {
+    console.error("load", error);
+  }
 );
 
 var renderer = new THREE.WebGLRenderer();
@@ -33,19 +34,36 @@ renderer.setClearColor(0xffff00);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var time;
+var k = window.innerWidth / window.innerHeight; //窗口宽高比
+var s = 2;//三维场景显示范围控制系数，系数越大，显示的范围越大
+var camera = new THREE.OrthographicCamera(-k * s, k * s, s, -s, 0.1, 1000);
+
+// camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+
+// 假设 x，y=1，0
+let x = 0;
+let y = 0;
+let z = 5;
+
+rotate = 1;
+
 var animate = function () {
   requestAnimationFrame(animate);
 
-  var delta = clock.getDelta();
-  for (var i = 0; i < mixers.length; i++) {
-    // 重复播放动画
-    mixers[i].update(delta);
-  }
+  rotate = rotate < 360 ? rotate + .05 : 0; // 旋转360°
 
-  //   stats.begin();
+  x = 5 * Math.sin(rotate);
+  z = 5 * Math.cos(rotate);
+
+  console.log("x", x)
+  console.log("z", z)
+  camera.position.x = x;
+  camera.position.z = z;
+
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
+
   renderer.render(scene, camera);
-  //   stats.end();
 };
 
 animate();
