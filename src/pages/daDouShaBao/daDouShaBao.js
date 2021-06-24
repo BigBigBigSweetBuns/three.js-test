3a6098266ae220d2ae13165c321a4c945532088b
BigBigBigSweetBuns <dadadadoushabao@gmail.com>
2021-06-24 19:18:26 +0800

    神奇的bug
    当镜头A，C组合能正反面
    当镜头B，D组合能正反面
    当A,B,C,D组就不能错位显示

--- src/pages/daDouShaBao/daDouShaBao.js
+++ src/pages/daDouShaBao/daDouShaBao.js
@@ -59,10 +59,14 @@ class PeppersGhostEffect {
         camera.position.z = z;
         camera.lookAt(scene.position);
       }
-      setCamera(_cameraA, 3, 0, 3); // 右上角
-      setCamera(_cameraB, -3, 0, 3); // 左上角
-      setCamera(_cameraD, -3, 0, 3); // 右下角
-      setCamera(_cameraC, 3, 0, 3); // 左下角
+      setCamera(_cameraA, 3, 0, 3); // 左上角 正面
+      setCamera(_cameraB, -3, 0, 3); // 左下角 背面
+      setCamera(_cameraC, -3, 0, 3); // 右上角 背面
+      setCamera(_cameraD, 3, 0, 3); // 右下角 正面面
+      // setCamera(_cameraA, 3, 0, 3); // 右45%
+      // setCamera(_cameraB, -3, 0, 3); // 背左45%
+      // setCamera(_cameraB, -3, 0, 3); // 背左45%
+      // setCamera(_cameraB, -3, 0, 3); // 背左45%
 
       renderer.clear();
       renderer.setScissorTest(true);
@@ -86,19 +90,11 @@ class PeppersGhostEffect {
       }
       const viewWidth = 400;
       const viewHeight = 400;
-      render(scene, _cameraA, 0, 0, 200, 210, 100, 100, viewWidth, viewHeight);
-      render(
-        scene,
-        _cameraB,
-        0,
-        0,
-        _width,
-        _height,
-        0,
-        0,
-        viewWidth,
-        viewHeight
-      );
+      // 尝试正面显示全部
+      render(scene, _cameraA, 100, 210, 100, 130, 0, 0, viewWidth, viewHeight); // 上 左
+      render(scene, _cameraB, 100, 80, 100, 130, 0, 0, viewWidth, viewHeight); // 下 左
+      render(scene, _cameraC, 200, 210, 100, 130, 0, 0, viewWidth, viewHeight); // 上 右
+      render(scene, _cameraD, 200, 80, 100, 130, 0, 0, viewWidth, viewHeight); // 下 右
     };
   }
 }
