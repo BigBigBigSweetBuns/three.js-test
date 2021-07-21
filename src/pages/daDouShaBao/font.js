410e30933745f97cab51fe79b88327e2357fb675
BigBigBigSweetBuns <dadadadoushabao@gmail.com>
2021-07-21 21:17:52 +0800

    优化fonts
    1.将3d文字对象从相对世界坐标改为相对自身
    2.将轴心从左下角 转移到物体中心点

--- src/pages/daDouShaBao/font.js
+++ src/pages/daDouShaBao/font.js
@@ -10,13 +10,13 @@ class TextGroup {
   flow4; // 270%
   translateXYZ = {
     x: 0,
-    y: -0.1,
-    z: -0.1,
+    y: 0,
+    z: 0,
   };
   translate2XYZ = {
-    x: -1.55,
-    y: -0.1,
-    z: -0.1,
+    x: 0,
+    y: 0,
+    z: 0,
   };
   constructor(
     text = "DaDouShaBao",
@@ -39,40 +39,40 @@ class TextGroup {
     const geometry2 = await this.initFont(this.text, this.fontFamily);
     const geometry3 = await this.initFont(this.text, this.fontFamily);
     const geometry4 = await this.initFont(this.text, this.fontFamily);
-    const textGeometry = this.initText(
-      geometry,
-      this.translateXYZ.x + textPosition.x,
-      this.translateXYZ.y,
-      this.translateXYZ.z,
-      rotateX1
-    );
-    const textGeometry2 = this.initText(
-      geometry2,
-      this.translate2XYZ.x + textPosition.x,
-      this.translate2XYZ.y,
-      this.translate2XYZ.z,
-      rotateX2
-    );
-    const textGeometry3 = this.initText(
-      geometry3,
-      this.translateXYZ.x + textPosition.x,
-      this.translateXYZ.y,
-      this.translateXYZ.z,
-      rotateX3
-    );
-    const textGeometry4 = this.initText(
-      geometry4,
-      this.translate2XYZ.x + textPosition.x,
-      this.translate2XYZ.y,
-      this.translate2XYZ.z,
-      rotateX4
-    );
+    const textGeometry = this.initText(geometry, rotateX1);
+    const textGeometry2 = this.initText(geometry2, rotateX2);
+    const textGeometry3 = this.initText(geometry3, rotateX3);
+    const textGeometry4 = this.initText(geometry4, rotateX4);
     const points = this.pointsXY(0.5, textPosition.x);
     const curve = this.initCurve(this.initMesh(points, 0.1, 0.1, 0.1));
-    this.flow = this.createFlow(textGeometry, curve);
-    this.flow2 = this.createFlow(textGeometry2, curve);
-    this.flow3 = this.createFlow(textGeometry3, curve);
-    this.flow4 = this.createFlow(textGeometry4, curve);
+    this.flow = this.createFlow(
+      textGeometry,
+      curve,
+      this.translateXYZ.x,
+      this.translateXYZ.y,
+      this.translateXYZ.z
+    );
+    this.flow2 = this.createFlow(
+      textGeometry2,
+      curve,
+      this.translate2XYZ.x,
+      this.translate2XYZ.y,
+      this.translate2XYZ.z
+    );
+    this.flow3 = this.createFlow(
+      textGeometry3,
+      curve,
+      this.translateXYZ.x,
+      this.translateXYZ.y,
+      this.translateXYZ.z
+    );
+    this.flow4 = this.createFlow(
+      textGeometry4,
+      curve,
+      this.translate2XYZ.x,
+      this.translate2XYZ.y,
+      this.translate2XYZ.z
+    );
     this.addLine(curve);
   }
   addLine(curve) {
@@ -180,16 +180,26 @@ class TextGroup {
     const geometry = await fontLoad(text, fontFamily);
     return geometry;
   }
-  initText(geometry, x, y, z, rotateX) {
-    geometry.translate(x, y, z);
+  /**
+   * 设置中心轴并旋转X轴
+   * @param {*} geometry
+   * @param {*} rotateX
+   * @returns
+   */
+  initText(geometry, rotateX) {
+    geometry.computeBoundingBox();
+    geometry.center();
     geometry.rotateX(rotateX);
     return geometry;
   }
-  createFlow(geometry, curve, color = 0x99ffff) {
+  createFlow(geometry, curve, x = 0, y = 0, z = 0, color = 0x99ffff) {
     const material = new THREE.MeshStandardMaterial({
       color,
     });
     const objectToCurve = new THREE.Mesh(geometry, material);
+    objectToCurve.position.x = x;
+    objectToCurve.position.y = y;
+    objectToCurve.position.z = z;
     const tempFlow = new Flow(objectToCurve);
     tempFlow.updateCurve(0, curve);
     return tempFlow;
